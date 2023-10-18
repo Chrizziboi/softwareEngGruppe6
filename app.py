ecbcecf0732fe99b2be08efcf2a1910d339a0c88
Christopher Fredh <88053773+Chrizziboi@users.noreply.github.com>
2023-10-18 09:06:08 +0200

    added method to change_user, create_table in db, register_user(wont be using probably) and started on login functionality

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
-
 import sqlite3
-
 from flask import Flask
 from backend.users.user import user
 from backend.users.admin import admin
 
-
 app = Flask(__name__)
-
 
 from flask import Flask, render_template
 
 DATABASE = 'chinook.db'
 
-
 app = Flask(__name__)
-
-def user_login():
-    pass
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 def get_db():
     db = getattr(Flask, '_database', None)
@@ -33,7 +20,47 @@ def get_db():
         db = Flask._database = sqlite3.connect(DATABASE)
     return db
 
-user1 = user(userID=int(1), username="user", login=True)
+def create_table():
+    conn = get_db()
+    conn.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)')
+
+def register_user(username, password):
+    conn = get_db()
+    conn.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+    conn.commit()
+    conn.close()
+
+def login(username, password):
+    conn = get_db()
+    cursor = conn.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+    user = cursor.fetchone()
+    conn.close()
+    return user
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = login(username, password)
+        if user:
+            session['user'] = user['username']
+            return f"Login Successful!\n Welcome {username}!, to a new expedition! <a href='/logout'>Logout</a>".format(user['username'])
+        else:
+            return 'Login failed! <a href="/index">Try again</a>'
+    return render_template('index.html')
+
+@app.route
+def change_user():
+    session.pop('user', None)
+    return "Logged out! <a href='/index'>Login again</a>"
+
+user1 = user(userID=int(1), username="user", password=True)
 print(user.get_user_info(user1))
 
 admin1 = admin(adminID=int(1), adminname="admin", login=True)
--- backend/users/user.py
+++ backend/users/user.py
@@ -4,10 +4,10 @@ from backend.login import login
 
 class user:
 
-    def __init__(self, userID, username, login):
+    def __init__(self, userID, username, password):
         self.userID = int(userID)
         self.username = str(username)
-        self.login = bool(login)
+        self.login = bool(password)
 
     def get_user_info(self):
         return f"UserID: {self.userID}, Username: {self.username}."
@@ -15,8 +15,8 @@ class user:
     def has_shopping_cart(self, shoppingCart):
         return shoppingCart is not None
 
-    def user_login(self, login):
-        if login == True:#alt; password == entered_password
+    def user_login(self, password):
+        if password == True:#alt; password == entered_password
             print("Sucessfully logged in.")
         else:
             print("couldn't log in.")
