d4a3c0f430bf88e46a60c9de7a1ae511b863688e
bragvin <tobyplays@icloud.com>
2023-10-19 10:49:53 +0200

    minor updates

--- app.py
+++ app.py
@@ -17,10 +17,6 @@ def user_login():
 def index():
     return render_template('index.html')
 
-@app.route('/user-page')
-def index():
-    return render_template('user-page.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
 
