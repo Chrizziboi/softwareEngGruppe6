99e06e2a53bbefe954759fb2bbb673578a507dff
Bragvin <131401239+bragvin@users.noreply.github.com>
2023-10-19 11:47:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import sqlite3
 
-from flask import Flask
+from flask import Flask, render_template
 from backend.users.user import user
 from backend.users.admin import admin
 
 app = Flask(__name__)
-
-from flask import Flask, render_template
 
 DATABASE = 'chinook.db'
 
