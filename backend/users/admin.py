0bae20ca4f3589b57209be60e6fc6dd60116f3dd
Christopher Fredh <88053773+Chrizziboi@users.noreply.github.com>
2023-10-19 11:28:13 +0200

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,15 +1,16 @@
 class admin:
-    def __init__(self, adminID, adminname, login):
+    def __init__(self, adminID, adminname): #parameter password for a system that would use password
         self.adminID = int(adminID)
         self.adminname = str(adminname)
-        self.login = bool(login)
+        #self.login = bool(login)
 
 
     def get_admin_info(self):
         return f"UserID: {self.adminID}, Username: {self.adminname}."
 
-    def admin_login(self, login):
+
+  '''  def admin_login(self, login):
         if login == True:#alt; password == entered_password
             print("Sucessfully logged in.")
         else:
-            print("couldn't log in.")
+            print("couldn't log in.")'''
