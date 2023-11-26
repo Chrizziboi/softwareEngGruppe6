db8ae7354737f73d535e5851258feec2e19be02e
Christopher Fredh <88053773+Chrizziboi@users.noreply.github.com>
2023-11-26 19:06:28 +0100

    added search-results.html and minor changes to user, shoppingCart.py, added search button functionality and search-results.html query page

--- schema.sql
+++ schema.sql
@@ -1,4 +1,15 @@
 
+CREATE TABLE IF NOT EXISTS peristorage (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userID INTEGER NOT NULL,
+    tour_name TEXT,
+    quantity INTEGER,
+    price REAL,
+    FOREIGN KEY (userID) REFERENCES users (id),
+    FOREIGN KEY (tour_name) REFERENCES tours (id)
+
+);
+
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     userID INTEGER NOT NULL,
@@ -10,17 +21,6 @@ CREATE TABLE IF NOT EXISTS tours (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     price REAL NOT NULL
-
-);
-
-CREATE TABLE IF NOT EXISTS peristorage (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    userID INTEGER NOT NULL,
-    tour_name TEXT,
-    quantity INTEGER,
-    price REAL,
-    FOREIGN KEY (userID) REFERENCES users (id),
-    FOREIGN KEY (tour_name) REFERENCES tours (id)
 
 );
 
